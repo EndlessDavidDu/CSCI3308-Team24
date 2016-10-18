5fb2399a836fb41cccd935099ff47d4cf694c039
Dingraham <dingraham01@gmail.com>
2016-10-18 16:55:35 -0600

    Create createTables.sql

---
+++ createTables.sql
@@ -0,0 +1,24 @@
+/* Purpose: Create the database and tables for the Rendezvous event app */
+/* Date: 10/17/2016 */
+CREATE DATABASE RVSQL;
+GO
+USE RVSQL;
+/* drop tables for now. Once we figure everything out we won't drop every time */
+DROP TABLE Event;
+DROP TABLE Customer;
+DROP TABLE CustEvent;
+DROP TABLE EventType;
+GO
+	
+CREATE TABLE Event (eventID int primary key, eventType int, description char(150), isClosed bool, startTime datetime, endTime datetime);
+CREATE TABLE Customer (custID int primary key, custName char(30), userEmail char(50), userPassword char(15));
+CREATE TABLE CustEvent (custeventID int primary key, eventID int, custID int); 
+CREATE TABLE EventType (typeID int primary key, description char(50));
+GO
+
+/* We will need to create more data for the other tables but this is a start for one of them  */
+INSERT INTO EventType VALUES (0, 'Business'); 
+INSERT INTO EventType VALUES (1, 'Music'); 
+INSERT INTO EventType VALUES (2, 'School'); 
+INSERT INTO EventType VALUES (3, 'Sports'); 
+GO
