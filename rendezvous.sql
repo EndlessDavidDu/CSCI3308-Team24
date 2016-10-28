9893618ec746706e913b4cfe04a79e2fac319b7a
tomeldar <tomeldar@users.noreply.github.com>
2016-10-27 20:38:40 -0600

    Filled Event table values

--- rendezvous.sql
+++ rendezvous.sql
@@ -31,10 +31,10 @@ INSERT INTO Customer VALUES (0004, 'Subi', 'subinair@colorado,edu', 'rendezvous_
 INSERT INTO Customer VALUES (0005, 'Izze', 'green@sparkling.gov', 'appleicious');
 
 INSERT INTO Event VALUES (1, 2, 'welcome to CU', false, '2016-10-27 10:00:00', '2016-10-27 12:00:00', 'cu boulder');
-INSERT INTO Event VALUES (2, 2, 'welcome to CU', false, '2016-10-27 10:00:00', '2016-10-27 12:00:00', 'cu boulder');
-INSERT INTO Event VALUES (3, 2, 'welcome to CU', false, '2016-10-27 10:00:00', '2016-10-27 12:00:00', 'cu boulder');
-INSERT INTO Event VALUES (4, 2, 'welcome to CU', false, '2016-10-27 10:00:00', '2016-10-27 12:00:00', 'cu boulder');
-INSERT INTO Event VALUES (5, 2, 'welcome to CU', false, '2016-10-27 10:00:00', '2016-10-27 12:00:00', 'cu boulder');
+INSERT INTO Event VALUES (2, 1, 'Riri', false, '2016-11-12 20:00:00', '2016-11-12 00:00:00', 'Pepsi Center');
+INSERT INTO Event VALUES (3, 1, 'Arctic Monkeys', false, '2016-12-27 17:00:00', '2016-12-27 20:00:00', 'Pepsi Center');
+INSERT INTO Event VALUES (4, 0, 'Soft Dev Meeting', false, '2016-10-27 18:00:00', '2016-10-27 20:00:00', 'CSEL');
+INSERT INTO Event VALUES (5, 3, 'Buffs Game vs UCLA', false, '2016-11-03 19:00:00', '2016-11-03 22:00:00', 'Folsom Field');
 
 INSERT INTO CustEvent VALUES (1, 1, 1);
 INSERT INTO CustEvent VALUES (2, 1, 1);
